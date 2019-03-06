95ff80fb761cf6847cf465452b042746171b0fb4
Tushar Jain <tusharlock10@gmail.com>
2019-03-06 20:36:33 +0530

    Worked on the basics

--- reciever.py
+++ reciever.py
@@ -1,7 +1,101 @@
-import os
+import os, random, zlib, json, tj, sys, time
 from socket import *
 
-DIRECTORY='Recieved'
+os.chdir(os.path.dirname(sys.argv[0]))
 
+DIRECTORY='Received'
 
 os.makedirs(DIRECTORY, exist_ok=1)
+
+
+class Receiver:
+    global DIRECTORY
+
+    def __init__(self):
+        print('  ** RECEVIER **')
+        print('This module will Receive files ...\n')
+
+        self.host=self.__get_host()
+        self.port=self.__get_port()
+        print('\nEnter both of these correctly in the sender...')
+        self.addr=(self.host, self.port)
+        self.buff=8*1024    # Its buffer is a little more than sender, to avoid the files getting corrupt
+        self.socket = socket(AF_INET, SOCK_DGRAM)
+        self.socket.bind(self.addr)
+        self.files=self.get_info()
+        self.wait_buffer=0.3
+
+    @staticmethod
+    def __get_host():
+        host=gethostname()
+        ip=gethostbyname(host)
+        print(f'The IP Address of Receiver is: {ip}')
+        return ip
+
+    @staticmethod
+    def __get_port():
+        port=random.choice(list(range(3000,30001)))
+        print(f'The port number is: {port}')
+        return port
+
+    def get_info(self):
+        '''Gets the files metadata from sender'''
+        data, addr = self.socket.recvfrom(self.buff)   # Here files is in cmp, bin, json
+        print('Got the metadata...')
+        data = zlib.decompress(data)   # bin, json
+        #print(data)
+        data=data.decode()  # json left
+        files=json.loads(data)  # files dictionary {file_1 : size_1,   file_2 : size_2 ...}
+
+        return files
+
+    def __get_file(self, file):
+        dirname=os.path.dirname(file)
+        os.makedirs(dirname, exist_ok=1)
+
+        tt=time.time()
+        f=open(file,'wb')
+        while 1:
+            data, addr = self.socket.recvfrom(self.buff)
+            f.write(data)
+            if data==b'':
+                break
+        f.close()
+
+        tt=(time.time()-tt)
+        size=self.files[file]
+
+        try:
+            speed=tj.convert_bytes(size//tt)
+            tt=round(tt,3)
+            s=f'took {tt}s, avg. speed - {speed}/s'
+        except:
+            s="file was small, didn't do measurements! "
+
+        if os.path.getsize(file)==self.files[file]:
+            error='  THE RECIEVED FILE WAS CORRUPT, RETRY FOR THIS FILE...'
+            error= tj.color_text(error, text_color='RED')
+        else:
+            error='  File is successfully recieved...'
+            error= tj.color_text(error, text_color='GREEN')
+
+        print(f' | Received, {s}', end='\n')
+        print(f'{error}')
+
+
+    def get_files(self):
+        for file in self.files:
+            print(f'Receiveing file: {file}',end='')
+            self.__get_file(file)
+
+    def close(self):
+        print('Files recieved, Closing the connection!')
+        self.socket.close()
+
+
+
+R=Receiver()
+R.get_files()
+R.close()
+
+
---
+++ sender.py
@@ -0,0 +1,162 @@
+import os, tj, zlib, sys, json, time
+from socket import *
+
+os.chdir(os.path.dirname(sys.argv[0]))
+
+DIRECTORY='Send'
+
+os.makedirs(DIRECTORY, exist_ok=1)
+
+class Sender:
+    global DIRECTORY
+
+    def __init__(self, files=None):
+        print('  ** SENDER **')
+        print('This module will send files to the receiver...\n')
+        self.host=self.__get_host()
+        self.port=self.__get_port()
+        self.files=self.get_files(files)    # A dictionary with names of all the files : file_size
+        self.addr=(self.host, self.port)
+        self.buff=8*1000    # Buffer, it should be 2*1024, but I found that 2*1000 in sender.py give less corrupt files
+        self.socket = socket(AF_INET, SOCK_DGRAM)
+        self.wait_buffer=0.3
+
+    @staticmethod
+    def __validate_ip(address):
+        try:
+            host_bytes = address.split('.')
+            valid = [int(b) for b in host_bytes]
+            valid = [b for b in valid if b >= 0 and b<=255]
+            return len(host_bytes) == 4 and len(valid) == 4
+        except:
+            return False
+
+    def __get_host(self):
+        msg='Enter the IP Address of the receiver: '
+        while True:
+            host=input(msg)
+            if host.upper() in ['Q','QUIT','E','EXIT']:
+                input('\n -- PRESS ENTER TO QUIT --')
+                sys.exit()
+
+            if not self.__validate_ip(host):
+                msg=f'{host} is not a correct IP Address, enter again carefully (enter Q to quit): '
+                continue
+            break
+        print(f'{host} seems to be a valid IP Address, connecting to it...\n')
+        return host
+
+    @staticmethod
+    def __get_port():
+        msg='Enter the port number: '
+        while True:
+            port=input(msg)
+            if port.upper() in ['Q','QUIT','E','EXIT']:
+                input('\n -- PRESS ENTER TO QUIT --')
+                sys.exit()
+
+            if not port.isdigit():
+                msg=='Enter only numbers (enter Q to quit): '
+                continue
+
+            port=int(port)
+            if not 3000<port<30000:
+                msg='Port number is invalid, enter correct port number (enter Q to quit): '
+                continue
+            break
+
+        print(f'{port} seems to be a valid port number, connecting to it...\n')
+        return port
+
+
+    def get_files(self, files):
+        if files!=None:
+            return {files:os.path.getsize(file)}
+
+        L=tj.get_files_in_folder(DIRECTORY)
+        D={}
+        for i in L:
+            file=i
+            size=os.path.getsize(file)
+            file=file.strip(os.path.dirname(DIRECTORY))
+            D.update({file:size})
+
+        return D
+
+    def send_info(self):
+        '''Sends the files metadata to the receiver'''
+        data=self.files
+        D={}    # Now replacing Send in file paths with Received
+        for file in data:
+            x=file
+            file_name=os.path.basename(x)
+            x=x.strip(file_name)
+            x=x.strip(os.path.basename(x))
+            x=os.pathh.join(x, 'Received')
+
+
+
+
+
+
+
+
+        data=json.dumps()
+        data=data.encode()
+        data=zlib.compress(data)
+        print('Starting transfer -')
+
+        time.sleep(self.wait_buffer)
+        self.socket.sendto(data, self.addr)
+        print('Metadata sent...')
+        time.sleep(self.wait_buffer)
+
+        if len(data)>512:time.sleep(self.wait_buffer*3)  # To make sure that the receiver had processed the metadata
+
+        if len(data)>1024:time.sleep(self.wait_buffer*6)
+
+
+    def __send_file(self, file):
+        '''A helper function for send_files'''
+        f=open(file,'rb')
+        t=time.time()
+        while True:
+            data=f.read(self.buff)  # data here will be binary, already
+            self.socket.sendto(data, self.addr)
+            if data==b'':
+                break
+        f.close()
+
+        tt=(time.time()-t)
+        size=self.files[file]
+
+        try:
+            speed=tj.convert_bytes(size//tt)
+            tt=round(tt,3)
+            s=f'took {tt}s, avg. speed - {speed}/s'
+        except:
+            s="file was small, didn't do measurements! "
+        
+        print(f' | Transferred, {s}', end='\n\n')
+
+        time.sleep(self.wait_buffer) # Wait between each new file call
+
+    def send_files(self):
+        for file in self.files:
+            print(f'Transferring file: {file}',end='')
+            self.__send_file(file)
+
+
+    def close(self):
+        print('Transfer complere, Closing the connection!')
+        self.socket.close()
+        input('Enter to quit...')
+
+
+S=Sender()
+S.send_info()
+S.send_files()
+S.close()
+
+
+
